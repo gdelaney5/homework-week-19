6bfe652658743eea16b3cdb0be3e37090aafff07
Garrett Delaney <gdelaney@wep-inc.com>
2020-04-05 17:45:57 -0700

    uploading SHIELD directory

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    this.setState({ employees });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>SHIELD Employees Directory</Title>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
